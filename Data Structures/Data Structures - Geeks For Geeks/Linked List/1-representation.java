75902c36b54bf2e37ce77beebbb5938684fe248b
Md. Alamin Mahamud <alamin.ineedahelp@gmail.com>
2016-07-27 01:20:40 +0600

    Linked List Representation in Java

---
+++ Data Structures/Data Structures - Geeks For Geeks/Linked List/1-representation.java
@@ -0,0 +1,19 @@
+class LinkedList
+{
+    // Head Node
+    Node head;
+
+    class Node
+    {
+        int data;
+        Node next;
+
+        // Constructor
+        // By Default next is initialized as NULL
+        Node(int d)
+        {
+            data = d;
+        } 
+    }
+
+}
