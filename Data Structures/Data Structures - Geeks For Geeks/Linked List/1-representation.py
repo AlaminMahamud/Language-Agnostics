0c87f7e3eac0ce3650ba182f4371f010e57b9fe3
Md. Alamin Mahamud <alamin.ineedahelp@gmail.com>
2016-07-27 01:27:34 +0600

    Linked List Representation in Python

---
+++ Data Structures/Data Structures - Geeks For Geeks/Linked List/1-representation.py
@@ -0,0 +1,15 @@
+# Node Class
+clas Node:
+
+    # Function to Initialize the Node Object
+    def __init__(self,data):
+        self.data = data # assigns data
+        self.next = None # Initialize next as null
+    
+
+# LinkedList Class
+class LinkedList:
+
+    # Function to  Initialize the LinkedList Object
+    def __init__(self):
+        self.head = None
